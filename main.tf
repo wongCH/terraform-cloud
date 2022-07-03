1a553b75f9c90c6840e93d0c3ca240a002ff4157
Wong Chuin Hun <wong.hun@gmail.com>
2022-07-03 13:18:06 +0800

    add 2 instance

--- main.tf
+++ main.tf
@@ -52,7 +52,7 @@ resource "aws_security_group" "vpc-web" {
 resource "aws_instance" "my-ec2-vm" {
   ami           = data.aws_ami.amzlinux.id 
   instance_type = var.instance_type
-  count = 1
+  count = 2
   key_name      = var.key_pair
   user_data = file("apache-install.sh")  
   vpc_security_group_ids = [aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id]
